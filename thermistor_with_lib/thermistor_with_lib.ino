42949b9445469c6dbf14186e1e78bfc9bba289c1
Benediktas Cicėnas <31150653+Bcicenas@users.noreply.github.com>
2024-02-11 18:28:20 +0200

    Add files via upload

---
+++ thermistor_with_lib/thermistor_with_lib.ino
@@ -0,0 +1,39 @@
+
+#include "thermistor.h"
+
+// Analog pin used to read the NTC
+#define NTC_PIN               A0
+
+// Thermistor object
+THERMISTOR thermistor(NTC_PIN,        // Analog pin
+                      7500,          // Nominal resistance at 25 ºC
+                      3950,           // thermistor's beta coefficient
+                      9950);         // Value of the series resistor
+
+// Global temperature reading
+uint16_t temp;
+
+/**
+ * setup
+ *
+ * Arduino setup function
+ */
+void setup()
+{
+  Serial.begin(9600);
+}
+
+/**
+ * loop
+ *
+ * Arduino main loop
+ */
+void loop()
+{
+  temp = thermistor.read();   // Read temperature
+
+  Serial.print("Temp in 1/10 ºC : ");
+  Serial.println(temp);
+
+  delay(5000);
+}
